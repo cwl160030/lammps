e6a608f90479b9705064d11d9060d244107905bd
jesse-dicenso <jdicenso1@outlook.com>
2025-06-12 13:11:31 -0500

    working qmmm

--- src/fix_qmhub.h
+++ src/fix_qmhub.h
@@ -16,11 +16,12 @@ class FixQmhub : public Fix {
   FixQmhub(class LAMMPS *, int, char **);
   ~FixQmhub() override;
   int setmask() override;
-  
-  void setup(int) override;		// send positions and charges to QMHub
-  void post_integrate() override;	//
+  void post_constructor() override;
 
+  void setup(int) override;		// send positions and charges to QMHub
+  void post_integrate() override;	// send positions and charges to QMHub
   void post_force(int) override;	// receive forces from QMHub
+  void end_of_step() override;
 
   void get_lmp_data(double *qm_coord,   // get positions, charges, and QM types
                     double *qm_chrgs, 
@@ -33,7 +34,11 @@ class FixQmhub : public Fix {
   int num_mm;				// Number of MM atoms
   int qm_r_chrg;			// Charge of QM region
   int qm_r_spin;			// Spin mulciplicity of QM region
-  int is_pbc;				// 0 -> no, 1 -> yes
+  int is_pbc;				// 0 -> no pbc, 1 -> pbc
+  int *atomic_numbers;			/* list of atomic numbers of atoms
+					   in the simulation; ordered so
+					   atomic_numbers[i] corresponds to
+					   atom type i */
 
   int groupbit_qm;			// Groupbit for region 'QM'
   int groupbit_mm;			// Groupbit for region 'MM'
